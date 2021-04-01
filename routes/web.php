08e17d1225a9a93b339efc937ad4d6748af49a74
ByMykel <bymykel5@gmail.com>
2021-04-01 19:26:53 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,14 @@
 <?php
 
+use App\Http\Controllers\LikeController;
 use App\Http\Controllers\ListMovieController;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\MovieController;
 use App\Http\Controllers\ReviewController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::post('movies/{movie}/reviews/store', [ReviewController::class, 'store'])->name('movies.reviews.store');
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
@@ -23,12 +24,13 @@ Route::get('/', [MovieController::class, 'home'])->name('home');
 Route::prefix('lists')->group(function () {
     Route::get('/', [ListMovieController::class, 'index'])->name('lists');
     Route::get('/create', [ListMovieController::class, 'create'])->name('lists.create')->middleware(['auth:sanctum', 'verified']);
+    Route::post('/store', [ListMovieController::class, 'store'])->name('lists.store')->middleware(['auth:sanctum', 'verified']);
 
     Route::prefix('{listMovie}')->group(function () {
         Route::get('/', [ListMovieController::class, 'show'])->name('lists.show');
 
         Route::middleware(['auth:sanctum', 'verified'])->group(function () {
-            Route::post('/edit', [ListMovieController::class, 'edit'])->name('lists.edit');
+            Route::get('/edit', [ListMovieController::class, 'edit'])->name('lists.edit');
             Route::post('/update', [ListMovieController::class, 'update'])->name('lists.update');
             Route::post('/destroy', [ListMovieController::class, 'destroy'])->name('lists.destroy');
         });
@@ -48,7 +50,7 @@ Route::prefix('movies')->group(function () {
 
         Route::prefix('reviews')->group(function () {
             Route::get('/', [ReviewController::class, 'index'])->name('movies.reviews.index');
-            Route::post('/store', [ReviewController::class, 'store'])->name('movies.reviews.store')->middleware(['auth:sanctum', 'verified']);;
+            Route::post('/store', [ReviewController::class, 'store'])->name('movies.reviews.store')->middleware(['auth:sanctum', 'verified']);
 
             Route::middleware(['auth:sanctum', 'verified'])->group(function () {
                 Route::prefix('{review}')->group(function () {
@@ -63,13 +65,12 @@ Route::prefix('movies')->group(function () {
 });
 
 Route::prefix('user')->group(function () {
-
     Route::prefix('{user:name}')->group(function () {
         Route::get('/', [UserController::class, 'show'])->name('user');
         Route::post('/follow', [UserController::class, 'follow'])->name('user.follow')->middleware(['auth:sanctum', 'verified']);
         Route::get('/following', [UserController::class, 'following'])->name('user.following');
         Route::get('/followers', [UserController::class, 'followers'])->name('user.followers');
-        Route::get('/movies', [MovieController::class, 'movies'])->name('user.movies');
+        Route::get('/watched', [MovieController::class, 'watched'])->name('user.watched');
         Route::get('/reviews', [ReviewController::class, 'reviews'])->name('user.reviews');
         Route::get('/lists', [ListMovieController::class, 'lists'])->name('user.lists');
 
