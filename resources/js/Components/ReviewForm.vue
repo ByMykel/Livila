49d2318c32bd78f2c55310c6357d69bacfb8e2ee
ByMykel <bymykel5@gmail.com>
2021-04-03 23:49:22 +0200

    Fix buttons when screen is small

--- resources/js/Components/ReviewForm.vue
+++ resources/js/Components/ReviewForm.vue
@@ -85,7 +85,7 @@
                         class="mr-2 inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                     >
                         <svg
-                            class="-ml-1 mr-2 h-5 w-5"
+                            class="sm:-ml-1 sm:mr-2 h-5 w-5"
                             fill="currentColor"
                             viewBox="0 0 20 20"
                             xmlns="http://www.w3.org/2000/svg"
@@ -96,7 +96,7 @@
                                 clip-rule="evenodd"
                             ></path>
                         </svg>
-                        Delete
+                        <span class="hidden sm:block">Delete</span>
                     </button>
 
                     <div>
@@ -107,7 +107,7 @@
                             class="mr-2 inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                         >
                             <svg
-                                class="-ml-1 mr-2 h-5 w-5 text-gray-500"
+                                class="sm:-ml-1 sm:mr-2 h-5 w-5 text-gray-500"
                                 xmlns="http://www.w3.org/2000/svg"
                                 viewBox="0 0 20 20"
                                 fill="currentColor"
@@ -117,7 +117,7 @@
                                     d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z"
                                 />
                             </svg>
-                            Edit
+                            <span class="hidden sm:block">Edit</span>
                         </button>
 
                         <button
@@ -127,7 +127,7 @@
                             class="mr-2 inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                         >
                             <svg
-                                class="-ml-1 mr-2 h-5 w-5"
+                                class="sm:-ml-1 sm:mr-2 h-5 w-5"
                                 fill="currentColor"
                                 viewBox="0 0 20 20"
                                 xmlns="http://www.w3.org/2000/svg"
@@ -138,7 +138,7 @@
                                     clip-rule="evenodd"
                                 ></path>
                             </svg>
-                            Cancel
+                            <span class="hidden sm:block">Cancel</span>
                         </button>
 
                         <button
@@ -147,7 +147,7 @@
                             class="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                         >
                             <svg
-                                class="-ml-1 mr-2 h-5 w-5"
+                                class="sm:-ml-1 sm:mr-2 h-5 w-5"
                                 xmlns="http://www.w3.org/2000/svg"
                                 viewBox="0 0 20 20"
                                 fill="currentColor"
@@ -159,7 +159,7 @@
                                     clip-rule="evenodd"
                                 />
                             </svg>
-                            Publish
+                            <span class="hidden sm:block">Publish</span>
                         </button>
                     </div>
                 </div>
