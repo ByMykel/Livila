668ef39ca2cd1ba5e6f863776a31b651a07e6d52
ByMykel <bymykel5@gmail.com>
2021-05-08 23:54:15 +0200

    refactor: change the max width of the navbar.

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <nav class="bg-black-500 relative z-10 shadow">
-            <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+            <div class="max-w-6xl mx-auto px-2 sm:px-6 lg:px-8">
                 <div class="relative flex items-center justify-between h-16">
                     <div
                         class="absolute inset-y-0 left-0 flex items-center sm:hidden"
