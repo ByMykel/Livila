79e06c3599a67019635e2a2159dd154eba512849
ByMykel <bymykel5@gmail.com>
2021-05-08 10:57:51 +0200

    refactor: refactor ReviewsNavbar and Movies/Show page.
    Change some style colors in ReviewsNavbar and add a reviews navbar to Movies/Show page.

--- resources/js/Components/ReviewsNavbar.vue
+++ resources/js/Components/ReviewsNavbar.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="border-b border-gray-600 mt-5 mb-5 text-gray-400 flex px-1">
+    <div class="border-b border-black-200 mt-5 mb-5 text-black-100 flex px-1">
         <a
             :href="route('movies.reviews.index', movie.id)"
-            class="hohover:border-b hover:text-white px-3 border-gray-500"
+            class="hover:border-b hover:text-white px-3 border-black-100"
             :class="{
                 'text-white border-indigo-600 border-b': route().current(
                     'movies.reviews.index',
@@ -13,7 +13,7 @@
         >
         <a
             :href="route('movies.reviews.popular', movie.id)"
-            class="hover:border-b hover:text-white px-3 border-gray-500"
+            class="hover:border-b hover:text-white px-3 border-black-100"
             :class="{
                 'text-white border-indigo-600 border-b': route().current(
                     'movies.reviews.popular',
@@ -24,7 +24,7 @@
         >
         <a
             :href="route('movies.reviews.friends', movie.id)"
-            class="hover:border-b hover:text-white px-3 border-gray-500"
+            class="hover:border-b hover:text-white px-3 border-black-100"
             :class="{
                 'text-white border-indigo-600 border-b': route().current(
                     'movies.reviews.friends',
--- resources/js/Pages/Movies/Show.vue
+++ resources/js/Pages/Movies/Show.vue
@@ -15,55 +15,84 @@
                 <movie-video :videos="movie.videos.results" />
 
                 <div
-                    v-if="friendsReviews.length > 0"
-                    class="border-b border-gray-600 mt-5 mb-2 text-white flex px-1"
+                    v-show="showReviewsNavbar"
+                    class="border-b border-black-200 mt-5 mb-5 text-black-100 flex justify-between px-1 cursor-pointer"
                 >
-                    <a
-                        :href="route('movies.reviews.friends', movie.id)"
-                        class="hover:text-indigo-400"
-                        >Friends reviews</a
-                    >
+                    <div class="flex">
+                        <a
+                            v-if="recentReviews.length"
+                            class="hover:border-b hover:text-white px-3 border-black-100"
+                            :class="{
+                                'text-white border-indigo-600 border-b':
+                                    selected === 1,
+                            }"
+                            @click="selected = 1"
+                            >Recent</a
+                        >
+                        <a
+                            v-if="popularReviews.length"
+                            class="hover:border-b hover:text-white px-3 border-black-100"
+                            :class="{
+                                'text-white border-indigo-600 border-b':
+                                    selected === 2,
+                            }"
+                            @click="selected = 2"
+                            >Popular</a
+                        >
+                        <a
+                            v-if="friendsReviews.length"
+                            class="hover:border-b hover:text-white px-3 border-black-100"
+                            :class="{
+                                'text-white border-indigo-600 border-b':
+                                    selected === 3,
+                            }"
+                            @click="selected = 3"
+                            >Friends</a
+                        >
+                    </div>
+                    <div class="flex items-center">
+                        <a :href="route('movies.reviews.index', movie.id)">
+                            <svg
+                                class="w-4 h-4 mx-3 text-white hover:text-indigo-400"
+                                fill="none"
+                                stroke="currentColor"
+                                viewBox="0 0 24 24"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    stroke-width="2"
+                                    d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+                                ></path>
+                            </svg>
+                        </a>
+                    </div>
                 </div>
 
-                <review-card
-                    v-for="review in friendsReviews"
-                    :key="'friend' + review.id"
-                    :review="review"
-                />
-
-                <div
-                    v-if="popularReviews.length > 0"
-                    class="border-b border-gray-600 mt-5 mb-2 text-white flex px-1"
-                >
-                    <a
-                        :href="route('movies.reviews.popular', movie.id)"
-                        class="hover:text-indigo-400"
-                        >Popular reviews</a
-                    >
+                <div v-if="selected === 1">
+                    <review-card
+                        v-for="review in recentReviews"
+                        :key="'recent' + review.id"
+                        :review="review"
+                    />
                 </div>
 
-                <review-card
-                    v-for="review in popularReviews"
-                    :key="'popular' + review.id"
-                    :review="review"
-                />
-
-                <div
-                    v-if="recentReviews.length > 0"
-                    class="border-b border-gray-600 mt-5 mb-2 text-white flex px-1"
-                >
-                    <a
-                        :href="route('movies.reviews.index', movie.id)"
-                        class="hover:text-indigo-400"
-                        >Recent reviews</a
-                    >
+                <div v-if="selected === 2">
+                    <review-card
+                        v-for="review in popularReviews"
+                        :key="'popular' + review.id"
+                        :review="review"
+                    />
                 </div>
 
-                <review-card
-                    v-for="review in recentReviews"
-                    :key="'recent' + review.id"
-                    :review="review"
-                />
+                <div v-if="selected === 3">
+                    <review-card
+                        v-for="review in friendsReviews"
+                        :key="'friend' + review.id"
+                        :review="review"
+                    />
+                </div>
             </div>
         </div>
     </app-layout>
@@ -94,6 +123,22 @@ export default {
         liked: Boolean,
         watched: Boolean,
         lists: Object,
+    },
+
+    data() {
+        return {
+            selected: 1,
+        };
+    },
+
+    computed: {
+        showReviewsNavbar() {
+            return (
+                this.friendsReviews.length ||
+                this.popularReviews.length ||
+                this.recentReviews.length
+            );
+        },
     },
 };
 </script>
