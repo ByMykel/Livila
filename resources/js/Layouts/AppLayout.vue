3e567ee46fb86d12782930ea6e454c03da07d03f
ByMykel <bymykel5@gmail.com>
2021-05-12 21:17:26 +0200

    feat: change logo and add favicon.

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -64,13 +64,13 @@
                     >
                         <div class="flex-shrink-0 flex items-center">
                             <img
-                                class="block lg:hidden h-8 w-auto"
-                                src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
+                                class="block lg:hidden h-9 w-auto"
+                                src="images/logo.svg"
                                 alt="Workflow"
                             />
                             <img
-                                class="hidden lg:block h-8 w-auto"
-                                src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg"
+                                class="hidden lg:block h-9 w-auto"
+                                src="images/logo-text.svg"
                                 alt="Workflow"
                             />
                         </div>
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        
+        <link rel="icon" href="{{ URL::asset('images/favicon.ico') }}" type="image/x-icon"/>
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
