072fff3508650623c368b06a4e3f188e3676f0e1
Mihail Dragnev <bymykel5@gmail.com>
2022-01-15 11:37:06 +0100

    refactor: change read more page.

--- resources/js/Pages/MoviesData.vue
+++ resources/js/Pages/MoviesData.vue
@@ -1,53 +1,41 @@
 <template>
-    <div class="font-sans text-white antialiased">
-        <div class="pt-4 bg-black-400">
-            <div class="min-h-screen flex flex-col items-center pt-6 sm:pt-0">
+    <div class="font-sans antialiased">
+        <div class="pt-4 bg-black-500">
+            <div class="flex flex-col items-center min-h-screen pt-6 sm:pt-0">
                 <div>
                     <jet-authentication-card-logo />
                 </div>
 
                 <div
-                    class="
-                        w-full
-                        sm:max-w-2xl
-                        mt-6
-                        p-6
-                        bg-white
-                        shadow-md
-                        overflow-hidden
-                        sm:rounded-lg
-                        prose
-                    "
+                    class="w-full p-6 mt-6 overflow-hidden shadow-md bg-black-400 sm:max-w-2xl sm:rounded-lg text-black-100"
                 >
                     <span>
                         All movies-related metadata used in this product is
                         supplied by The Movie Database (TMDb).
                     </span>
 
-                    <img
-                        class="block sm:hidden w-40 mx-auto"
-                        src="/images/blue_square_tmdb.svg"
-                    />
-                    <img
-                        class="hidden sm:block"
-                        src="/images/blue_long_tmdb.svg"
-                    />
+                    <div class="py-10">
+                        <img
+                            class="block w-40 mx-auto sm:hidden"
+                            src="/images/blue_square_tmdb.svg"
+                        />
+                        <img
+                            class="hidden sm:block"
+                            src="/images/blue_long_tmdb.svg"
+                        />
+                    </div>
 
-                    <!-- <span class="pt-2"
-                        >This product uses the TMDb API but is not endorsed or
-                        certified by TMDb.</span
-                    > -->
                     <span>
                         This product uses the
                         <a
-                            class="underline hover:text-indigo-400"
+                            class="underline text-black-200 hover:text-indigo-400"
                             href="https://developers.themoviedb.org/3/getting-started/introduction"
                             target="_blank"
                             >TMDb API</a
                         >
                         but is not endorsed or certified by
                         <a
-                            class="underline hover:text-indigo-400"
+                            class="underline text-black-200 hover:text-indigo-400"
                             href="https://www.themoviedb.org/"
                             target="_blank"
                             >TMDb</a
